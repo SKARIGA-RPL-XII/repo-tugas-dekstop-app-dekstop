5abc702831153687f8e3ec8e9cbd8a5dc220fb69
leeonata <aisyahnew08@gmail.com>
2026-01-19 10:07:07 +0700

    database

--- db_pos.sql
+++ db_pos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 11, 2026 at 01:52 PM
+-- Generation Time: Jan 19, 2026 at 03:04 AM
 -- Server version: 8.0.30
--- PHP Version: 8.3.11
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,11 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `id` int NOT NULL,
+  `nama_kasir` varchar(25) DEFAULT NULL,
+  `password` varchar(8) DEFAULT NULL,
+  `confirm_password` varchar(8) DEFAULT NULL,
+  `status` enum('active','non active') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`id`, `nama_kasir`, `password`, `confirm_password`, `status`) VALUES
+(1, 'H', 'H', NULL, 'active'),
+(2, 'Rafa', 'wwm', NULL, 'active');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `item`
 --
 
 CREATE TABLE `item` (
-  `No` int NOT NULL,
+  `kode_barang` int NOT NULL,
   `nama_item` varchar(50) NOT NULL,
   `kategori` varchar(50) NOT NULL,
   `harga` varchar(20) NOT NULL
@@ -38,29 +60,74 @@ CREATE TABLE `item` (
 -- Dumping data for table `item`
 --
 
-INSERT INTO `item` (`No`, `nama_item`, `kategori`, `harga`) VALUES
-(6, 'Mie Ayam', 'Makanan', '10000'),
-(7, 'Es Teh', 'Minuman', '4000');
+INSERT INTO `item` (`kode_barang`, `nama_item`, `kategori`, `harga`) VALUES
+(7, 'Es Teh', 'Minuman', '4000'),
+(8, 'Gorengan', 'Makanan', '10000'),
+(9, 'yyy', 'Makanan', '8000'),
+(10, 'kk', 'Minuman', '2000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int NOT NULL,
+  `no_faktur` varchar(20) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `nama_item` varchar(50) NOT NULL,
+  `quantity` int NOT NULL,
+  `harga` int NOT NULL,
+  `jumlah` int NOT NULL,
+  `total_bayar` int NOT NULL,
+  `cash` int NOT NULL,
+  `kembalian` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`No`);
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `No` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `kode_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
